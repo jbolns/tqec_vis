a5ac60b0f6de2b6a9a8bb4facb18f301b4365ee0
J <80164315+jbolns@users.noreply.github.com>
2025-01-23 20:10:10 +0200

    Animations & Primitive Controls

--- src/scripts/animation.js
+++ src/scripts/animation.js
@@ -1,30 +1,25 @@
 // ##################################
-// MAIN THREE IMPORTS
+// IMPORTS
 // ##################################
 //
 //
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-
-// ##################################
-// DATA
-// ##################################
-//
-//
 import * as IMPORTER from "./importer"
+import { materials, hadamardMaterials } from "./materials";
+import { updateKeyCache, onDoubleClick, onMouseDown, onContextMenu } from "./actions"
+import { cubeGeo } from "./primitives";
 
 // ##################################
-// GLOBAL DEFINITIONS
+// GLOBALS
 // ##################################
 //
 //
 let container;
 let camera, scene, renderer;
 let controls, group;
-let enableSelection = true;
-const objects = [];
-const mouse = new THREE.Vector2(), raycaster = new THREE.Raycaster();
+let objects = [];
 
 
 // ##################################
@@ -32,12 +27,12 @@ const mouse = new THREE.Vector2(), raycaster = new THREE.Raycaster();
 // ##################################
 //
 //
-
+// INITIALISE ANIMATION
 export default async function init(data_filename) {
 
+    // GET INITIAL GRAPH DATA
     const BLOCKS = await IMPORTER.get_blocks(data_filename)
     const EDGES = await IMPORTER.get_edges(data_filename, BLOCKS)
-
 
     // CREATE DOCUMENT ELEMENT
     const target = document.getElementById("animation-wrapper")
@@ -56,57 +51,19 @@ export default async function init(data_filename) {
 
     // ADD LIGHT
     const ambient = new THREE.AmbientLight(0xffffff)
-    const hemi = new THREE.HemisphereLight(0xffffff, 1);
-    hemi.position.set(10, 10, 10);
-    const spotlight1 = new THREE.SpotLight(0xffffff, 10000)
-    spotlight1.position.set(0, 25, 50);
-    spotlight1.castShadow = true;
-    scene.add(ambient, hemi, spotlight1);
-
-    // TURN CAMERAS ON
-    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 500);
-    camera.position.set(12, 9, 2);
+    scene.add(ambient);
 
     // ADD GRID
-    const grid = new THREE.GridHelper(50, 50, 0xff0000, 0xffffff)
+    const grid = new THREE.GridHelper(50, 50, 0x000000, 0x000000)
     grid.position.set(0.5, -0.5, 0.5)
-    const baseGeo = new THREE.PlaneGeometry(50, 50)
-    baseGeo.rotateX(- Math.PI / 2)
-    const baseMat = new THREE.MeshBasicMaterial({ color: 0x222222, side: THREE.DoubleSide })
-    const ground = new THREE.Mesh(baseGeo, baseMat)
-    ground.position.set(0, -0.5, 0)
-    scene.add(grid, ground)
+    scene.add(grid)
 
     // CREATE A GROUP IN CASE ITS NEEDED
     group = new THREE.Group();
     scene.add(group);
 
-    // CREATE BASE GEOMETRY FOR ALL BLOCKS
-    let geometry = new THREE.BoxGeometry();
-    const loader = new THREE.TextureLoader();
-    const texture_transp_face = loader.load('./src/assets/face_transparent.png');
-    const texture_blue_face = loader.load('./src/assets/face_blue.png');
-    const texture_red_face = loader.load('./src/assets/face_red.png');
-    const texture_hadamard_blue = loader.load('./src/assets/hadamard_blue.png');
-    const texture_hadamard_red = loader.load('./src/assets/hadamard_red.png');
-
-    let materials = [
-        new THREE.MeshBasicMaterial({ map: texture_blue_face, side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: texture_blue_face, side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: texture_transp_face, transparent: true, opacity: 0.4, side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: texture_transp_face, transparent: true, opacity: 0.4, side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: texture_red_face, side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: texture_red_face, side: THREE.DoubleSide }),
-    ];
-
-    let hadamard_materials = [
-        new THREE.MeshBasicMaterial({ map: texture_hadamard_blue, side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: texture_hadamard_blue, side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: texture_transp_face, transparent: true, opacity: 0.4, side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: texture_transp_face, transparent: true, opacity: 0.4, side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: texture_hadamard_red, side: THREE.DoubleSide }),
-        new THREE.MeshBasicMaterial({ map: texture_hadamard_red, side: THREE.DoubleSide }),
-    ]
+    // BASE GEOMETRIES & MATERIALS
+    // To keep file short, geometries & materials must be global or imported
 
     // RENDER NODES
     for (const key in BLOCKS) {
@@ -116,9 +73,9 @@ export default async function init(data_filename) {
         const type = BLOCKS[key]["type"]
 
         // Pick appropriate color/texture & define geometry
-        let object = new THREE.Mesh(geometry, materials);
+        let object = new THREE.Mesh(cubeGeo, materials);
         if (type === "hadamard") {
-            object = new THREE.Mesh(geometry, hadamard_materials);
+            object = new THREE.Mesh(cubeGeo, hadamardMaterials);
         }
 
         // Define object position
@@ -136,67 +93,76 @@ export default async function init(data_filename) {
     // RENDER EDGES
     for (const key in EDGES) {
         let [x, y, z, rot_x, rot_y, rot_z, scale_x, scale_y, scale_z] = Object.values(EDGES[key])
-        const color = 0x222222
-        const object = new THREE.Mesh(geometry, materials)
+        const object = new THREE.Mesh(cubeGeo, materials)
 
         // Define object position
         object.position.set(x, y, z);
         object.rotation.set(rot_x, rot_y, rot_z)
-        object.scale.set(scale_x, scale_y, scale_z)
+        object.scale.set(scale_x, 2, scale_z)
 
         // Add object to scene
-        scene.add(object);
+        //scene.add(object);
 
         // Add object to global objects array
-        objects.push(object);
+        //objects.push(object);
     }
+
+    // TURN CAMERAS ON (and position relative to structure size)
+    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 750);
+    const xx = objects.map(object => object.position.y)
+    const yy = objects.map(object => object.position.y)
+    camera.position.set(Math.max(...xx) + 2, Math.max(...yy) + 2, 2);
 
     // ORBIT CONTROLS
     controls = new OrbitControls(camera, renderer.domElement);
-    controls.addEventListener('change', render);  // re-render on change
+    controls.addEventListener("change", render);  // re-render on change
+
+    // BUNCH OF EVENT LISTENERS
+    window.addEventListener("resize", onWindowResize);  // Adjust camera, render on window resize
+    document.addEventListener("dblclick", (event) => onDoubleClick(scene, camera, objects, event));  // Fires on double click
+    document.addEventListener("mousedown", (event) => onMouseDown(scene, objects, event)) // Fires on single click
+    document.addEventListener("contextmenu", onContextMenu);
+    document.addEventListener("keydown", updateKeyCache) // Fires on pressing any key
+    document.addEventListener("keyup", updateKeyCache) // Fires on pressing any key
+    
 
 
-    // Bunch of event listeners
-    window.addEventListener('resize', onWindowResize);  // Adjust camera, render on window resize
-
-    // Render
-    startStopAnimFrame()
+    // RENDER
+    startFrame()
 }
 
 
 // ##################################
-// FUNCTIONS NEEDED FOR INTERACTIVITY
+// STANDARD INTERACTIVITY OPERATIONS
 // ##################################
 //
 //
+// HANDLE WINDOW RESIZING
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
     render();
-
 }
 
+// CALL THREEJS RENDERER
 function render() {
     renderer.render(scene, camera);
-    scene.rotation.y -= 0.001
+
 }
 
-function startStopAnimFrame() {
+// START THREEJS ANIMATION
+function startFrame() {
     var id
     async function animate() {
         id = requestAnimationFrame(animate)
-        scene.rotation.y -= 0.0005
         renderer.render(scene, camera)
     }
     animate()
-    setTimeout(() => {
-        cancelAnimationFrame(id)
-    }, "1000");
 }
 
-
-function dispose() {
+// DISPOSE OF THREEJS SCENE
+function disposeScene() {
     var to_remove = []
     scene.traverse(child => {
         to_remove.push(child);
@@ -209,5 +175,3 @@ function dispose() {
     renderer.dispose()
     scene.clear()
 }
-
-export { dispose }
--- src/scripts/importer.js
+++ src/scripts/importer.js
@@ -1,17 +1,31 @@
+
+// ##################################
+// FETCH
+// ##################################
+//
+//
+// GET RAW DATA FROM JSON
 const get_data = async (data_filename) => {
   const response = await fetch(`../../data/${data_filename}`);
   const data = await response.json();
   return data
 }
 
+// ##################################
+// FORMAT
+// ##################################
+//
+//
+// EXTRACT NODES FROM DATA
 const get_blocks = async (data_filename) => {
-  
-  // Fetch & extract foundational data
+
+  // FETCH & EXTRACT FROM FOUNDATIONAL DATA
   const data = await get_data(data_filename)
   const wire_vertices = data["wire_vertices"]
   const nodes = data["node_vertices"]
-  
-  // Array to hold joint nodes
+
+  // POPULATE ARRAY WITH NODES
+  // Dictionary to hold final nodes
   let B = {}
 
   // Get main blocks
@@ -26,18 +40,19 @@ const get_blocks = async (data_filename) => {
     B[key] = { "type": "boundary", "coord": entries[0][1]["coord"] }
   }
 
-  // Return joint result
+  // RETURN JOINT RESULT
   return B
 }
 
-
+// EXTRACT VERTICES FROM DATA
 const get_edges = async (data_filename, BLOCKS) => {
-  
-  // Fetch & extract foundational data
+
+  // FETCH & EXTRACT FROM FOUNDATIONAL DATA
   const data = await get_data(data_filename)
   const edges = data["undir_edges"]
 
-  // Array to hold edges
+  // POPULATE ARRAY WITH EDGES
+  // Array to hold final edges
   const E = {}
 
   // Get edges and calculate coordinates
@@ -59,15 +74,21 @@ const get_edges = async (data_filename, BLOCKS) => {
       "rotation_x": BLOCKS[entries[0][1]]["coord"]["rotation_x"],
       "rotation_y": BLOCKS[entries[0][1]]["coord"]["rotation_y"],
       "rotation_z": BLOCKS[entries[0][1]]["coord"]["rotation_z"],
-      "scale_x": Math.max(0.9, delta_x),
-      "scale_y": Math.max(0.9, delta_y),
-      "scale_z": Math.max(0.9, delta_z),
+      "scale_x": Math.max(1, delta_x),
+      "scale_y": Math.max(1, delta_y),
+      "scale_z": Math.max(1, delta_z),
     }
   }
 
-  // Return joint result
+  // RETURN JOINT RESULT
   return E
 }
 
+
+// ##################################
+// EXPORTS
+// ##################################
+//
+//
 export { get_blocks, get_edges }
 
